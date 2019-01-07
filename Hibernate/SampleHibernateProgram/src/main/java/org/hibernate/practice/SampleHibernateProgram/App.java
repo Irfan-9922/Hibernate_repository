71dcea84a566f54af7caa3b9a76bab3d2f10ecc6
Irfan <Irfan@LAPTOP-18S3FPLI>
2019-01-08 01:17:09 +0530

    Letest Changes with Comments And maintain coding Standerds.

--- Hibernate/SampleHibernateProgram/src/main/java/org/hibernate/practice/SampleHibernateProgram/App.java
+++ Hibernate/SampleHibernateProgram/src/main/java/org/hibernate/practice/SampleHibernateProgram/App.java
@@ -4,24 +4,26 @@ import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
 /**
- * Hello world!
+ * @author Irfan
  *
  */
 public class App {
 	public static void main(String[] args) {
-	//	Session session = null;
+		// Transient Object.
 		Employee employee = new Employee();
-		
 		employee.setEmployeeSalery(12039L);
 		employee.setEmpolyeeName("Irfan");
+		// Session
+		Session session = null;
 		try {
-			Session	session = new Configuration().configure().buildSessionFactory().openSession();
+			session = new Configuration().configure().buildSessionFactory().openSession();
 			session.beginTransaction();
 			session.save(employee);
 			System.out.println("data saved \t" + employee.getEmployeeId());
 			session.getTransaction().commit();
 		} catch (Exception e) {
-			//session.close();
+			session.getTransaction().rollback();
+			session.close();
 		}
 
 	}
--- Hibernate/SampleHibernateProgram/src/main/java/org/hibernate/practice/SampleHibernateProgram/Employee.java
+++ Hibernate/SampleHibernateProgram/src/main/java/org/hibernate/practice/SampleHibernateProgram/Employee.java
@@ -5,34 +5,70 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+/**
+ * @author Irfan
+ *
+ */
 @Entity
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	/**
+	 The employeeId
+	 */
 	private Long employeeId;
-	@Override
-	public String toString() {
-		return "Employee [employeeId=" + employeeId + ", empolyeeName=" + empolyeeName + ", employeeSalery="
-				+ employeeSalery + "]";
-	}
+	
+	/**
+	 * The empolyeeName
+	 */
 	private String empolyeeName;
+	
+	/**
+	 * The employeeSalery
+	 */
 	private Long employeeSalery;
+
+	/**
+	 * @return employeeId
+	 */
 	public Long getEmployeeId() {
 		return employeeId;
 	}
+
+	/**
+	 * To Set EmployeeId
+	 * @param employeeId
+	 */
 	public void setEmployeeId(Long employeeId) {
 		this.employeeId = employeeId;
 	}
+
+	/**
+	 * @return empolyeeName
+	 */
 	public String getEmpolyeeName() {
 		return empolyeeName;
 	}
+
+	/**
+	 * To set EmpolyeeName
+	 * @param empolyeeName
+	 */
 	public void setEmpolyeeName(String empolyeeName) {
 		this.empolyeeName = empolyeeName;
 	}
+
+	/**
+	 * @return employeeSalery
+	 */
 	public Long getEmployeeSalery() {
 		return employeeSalery;
 	}
+
+	/**
+	 * To set EmployeeSalery
+	 * @param employeeSalery
+	 */
 	public void setEmployeeSalery(Long employeeSalery) {
 		this.employeeSalery = employeeSalery;
 	}
